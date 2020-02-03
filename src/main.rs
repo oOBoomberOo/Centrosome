ca4702a8be207f80ad3344e365c1b6536550d98a
Boomber <boombercmd123@gmail.com>
2020-02-04 00:27:32 +0700

    GUI v1

---
+++ src/main.rs
@@ -0,0 +1,112 @@
+use clap::{App, Arg};
+use colored::*;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use rayon::prelude::*;
+
+use dialoguer::{Select};
+use dialoguer::theme::ColorfulTheme;
+
+use std::fs::{DirEntry, remove_dir_all};
+use std::path::{Path};
+use std::thread;
+
+mod zip_peaker;
+
+fn main() {
+	let matches = App::new("datapack-merger")
+		.version("0.0.1")
+		.about("{Program Name} is a datapack merger tool")
+		.author("Boomber <boomberisalreadytaken@gmail.com>")
+		.arg(
+			Arg::with_name("directory")
+				.short("d")
+				.long("directory")
+				.required(true)
+				.index(1)
+				.help("Path to directory containing datapacks to be merge"),
+		)
+		.get_matches();
+
+	let directory = matches.value_of("directory").unwrap();
+	let directory = Path::new(directory);
+
+	if directory.exists() {
+		if directory.is_dir() {
+			merge(&directory);
+		} else {
+			eprintln!(
+				"'{}' is not a directory",
+				format!("{}", directory.display()).cyan()
+			);
+		}
+	} else {
+		eprintln!(
+			"'{}' does not exist.",
+			format!("{}", directory.display()).cyan()
+		);
+	}
+}
+
+fn merge(directory: &Path) {
+	let temp_dir = std::env::temp_dir().join("datapack-merger");
+
+	// Return all zip files in `directory`
+	let result: Vec<DirEntry> = directory
+		.read_dir()
+		.unwrap()
+		.filter_map(|entry| entry.ok())
+		// .filter(|entry: &DirEntry| entry.path().is_file() && entry.path().ends_with(".zip"))
+		.collect();
+
+	let selection_items: Vec<String> = result.iter().map(|entry: &DirEntry| entry.file_name().to_str().unwrap().to_string()).collect();
+
+	let _selection = Select::with_theme(&ColorfulTheme::default())
+		.with_prompt("Please choose core datapack")
+		.default(0)
+		.items(&selection_items)
+		.interact().unwrap();
+
+	let progress_bars = MultiProgress::new();
+
+	// Map all "datapack names" to numerical value
+	let mut name_lengths: Vec<usize> = result.iter()
+		.map(|entry| entry.file_name().to_str().unwrap().to_string().len())
+		.collect();
+	// Sort it to quickly get the highest value
+	name_lengths.sort();
+	let max_length = name_lengths.last().unwrap();
+
+	// Create progress bars *before* running .par_iter() because that's when thread blocking happen.
+	let result_with_progress_bars: Vec<(DirEntry, ProgressBar)> = result
+		.into_iter()
+		.map(|entry| {
+			let progress_bar = create_progress_bar(&progress_bars, &entry, *max_length);
+			(entry, progress_bar)
+		})
+		.collect();
+
+	// MultiProgress have to be run in another thread so that .par_iter() won't block each other process.
+	thread::spawn(move || {
+		progress_bars.join().unwrap();
+	});
+
+	result_with_progress_bars
+		.par_iter()
+		// .iter()
+		.for_each(|(entry, progress_bar)| {
+			zip_peaker::peak(entry, &temp_dir, &progress_bar).unwrap();
+		});
+
+	remove_dir_all(&temp_dir).unwrap();
+}
+
+fn create_progress_bar(multi_progress: &MultiProgress, entry: &DirEntry, width: usize) -> ProgressBar {
+	let entry_name = entry.file_name().to_str().unwrap().bright_green().bold();
+	let template = format!("[{{elapsed}}] {:<width$} [{{wide_bar:.cyan/white}}] {{pos:>4}}/{{len:4}} {{percent:>3}}% ({{eta:^4}})", entry_name, width = width);
+	let progress_bar = ProgressBar::new(1000)
+		.with_style(ProgressStyle::default_bar()
+			.template(&template)
+			.progress_chars("#>-")
+		);
+	multi_progress.add(progress_bar)
+}
